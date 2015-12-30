4b3db79c676978a7b975de02239b46358954497d
Anna Karingal <anna.karingal@gmail.com>
2015-12-30 13:02:58 -0500

    remove submenu

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -9,17 +9,6 @@
     <?php foreach($pages->visible() as $p): ?>
     <li>
       <a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
-
-      <?php if($p->hasVisibleChildren() && $p->id()!= "graphic-design"): ?>
-      <ul class="submenu">
-        <?php foreach($p->children()->visible() as $p): ?>
-        <li>
-          <a href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
-        </li>
-        <?php endforeach ?>
-      </ul>
-      <?php endif ?>
-
     </li>
     <?php endforeach ?>
   </ul>
